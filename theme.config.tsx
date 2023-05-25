e9e544d241162a3b4a5930e1e96adb7d5708a8de
Marcel Thomas <marcelthomas5@gmail.com>
2023-05-26 00:36:38 +0100

    Added favicon

--- theme.config.tsx
+++ theme.config.tsx
@@ -13,6 +13,28 @@ const config: DocsThemeConfig = {
       titleTemplate: '%s - SellerAssist',
     };
   },
+  head: (
+    <>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+    </>
+  ),
   footer: {
     text: (
       <span>
